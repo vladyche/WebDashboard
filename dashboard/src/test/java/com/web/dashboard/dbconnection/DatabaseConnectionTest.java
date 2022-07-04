ae6f50f961a8c1badef0be7120f8d9842b220530
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2022-07-05 01:38:06 +0300

    test connection to the database use Hibernate

--- dashboard/src/test/java/com/web/dashboard/dbconnection/DatabaseConnectionTest.java
+++ dashboard/src/test/java/com/web/dashboard/dbconnection/DatabaseConnectionTest.java
@@ -4,15 +4,18 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.junit.jupiter.api.Test;
 
 public class DatabaseConnectionTest {
+	private String url = "jdbc:postgresql://localhost:5432/test_web_dashboard";
+	private String user = "postgres";
+	private String pass = "pg123";
 	
 	@Test
 	void jdbcConnection() {
-		String url = "jdbc:postgresql://localhost:5432/test_web_dashboard";
-		String user = "postgres";
-		String pass = "pg123";
 		
 		try {
 			System.out.println("Connection...");
@@ -20,6 +23,22 @@ public class DatabaseConnectionTest {
 			System.out.println("Connect! " + connect);
 		} catch (SQLException e) {
 			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	void hibernateConnection() {
+		SessionFactory sessionFactory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.buildSessionFactory();
+		
+		Session session = sessionFactory.getCurrentSession();
+		
+		try {
+			System.out.println("Connect " + session);
+		}finally {
+			sessionFactory.close();
+			System.out.println("Closed!");
 		}
 	}
 }
